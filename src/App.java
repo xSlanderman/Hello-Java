d2eb0acfda63784428ffb1bd97befb4e1fc504c7
xSlanderman <boraliviucalin@hotmail.com>
2025-02-17 00:56:38 +0200

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("\n Hello, World! \n");
+
+        System.out.println("Hello Again");
     }
 }
